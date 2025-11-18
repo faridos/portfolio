520cdde05f99422e5b0fd6281e4e50850422e170
farid maghraoui <fm@merkle.media>
2025-11-18 23:26:33 +0100

    FIX REACT_APP_API_URL

--- backend/start.sh
+++ backend/start.sh
@@ -18,7 +18,7 @@ python create_admin.py
 # Start the application
 echo "Starting application server..."
 if [ "$ENVIRONMENT" = "production" ]; then
-    exec gunicorn app.main:app -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
+    exec gunicorn app.main:app --proxy-headers --forwarded-allow-ips="*"  -w 4 -k uvicorn.workers.UvicornWorker -b 0.0.0.0:8000
 else
     exec uvicorn app.main:app --proxy-headers --forwarded-allow-ips="*" --host 0.0.0.0 --port 8000
 fi 
