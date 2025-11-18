e9f2efb7596fc6544b60bca211c934dcb5967146
farid maghraoui <fm@merkle.media>
2025-11-18 22:44:10 +0100

    FIX REACT_APP_API_URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -46,6 +46,7 @@ templates = Jinja2Templates(directory="app/templates")
 # Configure CORS
 origins = [
     "http://localhost:3000",
+    "https://apiportfolio.madeingermany.tn",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
