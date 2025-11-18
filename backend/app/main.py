ef01c803b29a1d68633f504eee0de31faac5688c
farid maghraoui <fm@merkle.media>
2025-11-18 22:56:10 +0100

    FIX REACT_APP_API_URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -47,6 +47,7 @@ templates = Jinja2Templates(directory="app/templates")
 origins = [
     "http://localhost:3000",
     "https://apiportfolio.madeingermany.tn",
+    "https://farid-maghraoui.madeingermany.tn",
     "http://localhost:8000",
     "http://127.0.0.1:3000",
     "http://127.0.0.1:8000",
